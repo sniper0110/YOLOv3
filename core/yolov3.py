f3c9b700ef7fda1ed8a22c5d8084b27986c9380e
sniper0110 <nourislam.mokhtari@gmail.com>
2021-01-12 19:18:55 +0100

    decreasing the initial learning rate to avoid nan values

--- core/yolov3.py
+++ core/yolov3.py
@@ -114,7 +114,7 @@ def bbox_iou(boxes1, boxes2):
 
     inter_section = tf.maximum(right_down - left_up, 0.0)
     inter_area = inter_section[..., 0] * inter_section[..., 1]
-    union_area = boxes1_area + boxes2_area - inter_area + 1e-10
+    union_area = boxes1_area + boxes2_area - inter_area + 5e-10
 
     return 1.0 * inter_area / union_area
 
@@ -144,7 +144,7 @@ def bbox_giou(boxes1, boxes2):
     enclose_left_up = tf.minimum(boxes1[..., :2], boxes2[..., :2])
     enclose_right_down = tf.maximum(boxes1[..., 2:], boxes2[..., 2:])
     enclose = tf.maximum(enclose_right_down - enclose_left_up, 0.0)
-    enclose_area = enclose[..., 0] * enclose[..., 1] + + 1e-10
+    enclose_area = enclose[..., 0] * enclose[..., 1] + 1e-10
     giou = iou - 1.0 * (enclose_area - union_area) / enclose_area
 
     return giou
